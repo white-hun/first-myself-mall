c105a51bd3618c54c827ced50a4c03b56aec30b1
white-hun <exwhite@naver.com>
2023-04-19 22:10:38 +0900

    Update

--- first-myself-mall/src/components/Header.jsx
+++ first-myself-mall/src/components/Header.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Login from "./Login";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const hanbleClick = () => navigate("/product");
   return (
     <header>
       <Link to="/">
         <p>shoppy</p>
       </Link>
       <div>
-        <p>products</p>
-        <p>장바구니</p>
-        <Login />
+        <button onClick={hanbleClick}>products</button>
+        <button>장바구니</button>
+        <div>
+          <Login />
+        </div>
       </div>
     </header>
   );
