2be746eddac25d2cc7f4e158c07778ca0facbdf0
white-hun <exwhite@naver.com>
2023-04-22 22:01:34 +0900

    Update

--- first-myself-mall/src/service/Login.jsx
+++ first-myself-mall/src/service/Login.jsx
@@ -17,6 +17,7 @@ export default function Login() {
   const handleGoogleLogOut = () => {
     const auth = getAuth();
     signOut(auth);
+    setUserData(null);
   };
   const handleNewProduct = () => {
     navigate("/products/new");
