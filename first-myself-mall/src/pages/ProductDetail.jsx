bf52c19c8a40c13feeaa6f9f62bdd104200232a7
white-hun <exwhite@naver.com>
2023-04-23 17:15:41 +0900

    Update ProductDetail.jsx

--- first-myself-mall/src/pages/ProductDetail.jsx
+++ first-myself-mall/src/pages/ProductDetail.jsx
@@ -18,11 +18,19 @@ export default function ProductDetail() {
         <label htmlFor="size"></label>
         <select id="size">
           <option value="">--pleae choose a size--</option>
-          {size.option.map((s) => (
-            <option>{s}</option>
-          ))}
+          <option value="">{size.option.small}</option>
+          <option value="">{size.option.medium}</option>
+          <option value="">{size.option.large}</option>
+          <option value="">{size.option.xlarge}</option>
+          <option value="">{size.option.xxlarge}</option>
         </select>
       </article>
     </section>
   );
+}
+
+{
+  /* {size.option.map((s) => (
+  <option value={product.id}>{s}</option>
+))} */
 }
