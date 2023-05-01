8060ee707a1999066fcecf8e0c20c94d313939a0
white-hun <121800818+white-hun@users.noreply.github.com>
2023-05-01 10:24:02 +0900

    Update NewProduct

--- first-myself-mall/src/pages/NewProduct.jsx
+++ first-myself-mall/src/pages/NewProduct.jsx
@@ -5,11 +5,23 @@ import { db } from "../service/firebaseConfig";
 
 export default function NewProduct() {
   const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [kind, setKind] = useState("");
+  const [size, setSize] = useState("");
+  const [description, setDescription] = useState("");
   const handleTitle = (e) => setTitle(e.target.value);
+  const handlePrice = (e) => setPrice(e.target.value);
+  const handleKind = (e) => setKind(e.target.value);
+  const handleSize = (e) => setSize(e.target.value);
+  const handleDescription = (e) => setDescription(e.target.value);
   const board = collection(db, "board");
   const setBoard = async () =>
-    await setDoc(doc(board, "TST"), {
+    await setDoc(doc(board, "items"), {
       name: title,
+      price: price,
+      category: kind,
+      size: { default: size },
+      description: description,
     });
 
   const handleSubmit = (e) => {
@@ -17,12 +29,19 @@ export default function NewProduct() {
     setBoard();
     console.log(board);
     setTitle("");
+    setPrice("");
+    setKind("");
+    setSize("");
+    setDescription("");
   };
   return (
     <form onSubmit={handleSubmit}>
       <h2>새로운 제품 등록</h2>
       <input type="text" placeholder="제품명" onChange={handleTitle} value={title} />
-
+      <input type="text" placeholder="가격" onChange={handlePrice} value={price} />
+      <input type="text" placeholder="카테고리" onChange={handleKind} value={kind} />
+      <input type="text" placeholder="사이즈" onChange={handleSize} value={size} />
+      <input type="text" placeholder="제품설명" onChange={handleDescription} value={description} />
       <button>제품 등록하기</button>
     </form>
   );
