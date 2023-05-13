b1c2ca50d5beb229be5b4e27e722612fe1b4d6b8
white-hun <exwhite@naver.com>
2023-05-13 19:27:12 +0900

    Update

--- first-myself-mall/src/pages/MyCart.jsx
+++ first-myself-mall/src/pages/MyCart.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../service/firebaseConfig";
+import CartProduct from "../components/CartProduct";
 
 export default function MyCart() {
-  return <div>MyCart</div>;
+  const [cartProd, setCartProd] = useState([]);
+  useEffect(() => {
+    const getProd = async () => {
+      const querySnapshot = await getDocs(collection(db, "board"));
+      setCartProd(querySnapshot.docs.map((doc) => ({ ...doc.data() })));
+    };
+    getProd();
+  }, []);
+
+  return (
+    <>
+      {console.log(cartProd)}
+      {cartProd && (
+        <ul>
+          {cartProd.map((item) => (
+            <CartProduct key={item.id} product={item} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
