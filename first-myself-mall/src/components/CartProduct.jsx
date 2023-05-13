dec5b1df3c8e001cf10171afd78c8464d136d9bd
white-hun <exwhite@naver.com>
2023-05-13 21:43:39 +0900

    Update cart

--- first-myself-mall/src/components/CartProduct.jsx
+++ first-myself-mall/src/components/CartProduct.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function CartProduct() {
-  return <div></div>;
+export default function CartProduct({ product }) {
+  const { name, price, category, size, quantity, imageurl } = product;
+  return (
+    <div>
+      {/* <img src={imageurl} alt={name} /> */}
+      <p>{name}</p>
+      <div>{price}</div>
+      <div>{category}</div>
+    </div>
+  );
 }
--- first-myself-mall/src/pages/MyCart.jsx
+++ first-myself-mall/src/pages/MyCart.jsx
@@ -1,13 +1,13 @@
-import { collection, getDocs } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
-import { db } from "../service/firebaseConfig";
 import CartProduct from "../components/CartProduct";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../service/firebaseConfig";
 
 export default function MyCart() {
   const [cartProd, setCartProd] = useState([]);
   useEffect(() => {
     const getProd = async () => {
-      const querySnapshot = await getDocs(collection(db, "board"));
+      const querySnapshot = await getDocs(collection(db, "board", "basket"));
       setCartProd(querySnapshot.docs.map((doc) => ({ ...doc.data() })));
     };
     getProd();
