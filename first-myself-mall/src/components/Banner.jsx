b6ba09841635940d5cc82a54a375f1be725838e8
white-hun <121800818+white-hun@users.noreply.github.com>
2023-05-29 18:45:29 +0900

    Create Banner.jsx

---
+++ first-myself-mall/src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// update
+
+export default function Banner() {
+  return (
+    <div className="m-5">
+      <img src="/image/banner1.jpg" alt="bannerImage" />
+    </div>
+  );
+}
