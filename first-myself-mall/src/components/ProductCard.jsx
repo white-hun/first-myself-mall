761c5c6caab9e4d0f4e19664d2202a2ac59d0d7f
white-hun <121800818+white-hun@users.noreply.github.com>
2023-06-07 12:01:03 +0900

    Update ProductCard.jsx

--- first-myself-mall/src/components/ProductCard.jsx
+++ first-myself-mall/src/components/ProductCard.jsx
@@ -7,7 +7,7 @@ export default function ProductCard({ product }) {
   const handleClick = () => navigate(`/products/${product.id}`, { state: { product } });
 
   return (
-    <div onClick={handleClick} className="w-64 mb-3 bg-gray-50 rounded-xl p-2">
+    <div onClick={handleClick} className="w-64 mb-3 bg-gray-50 rounded-lg p-2">
       <img src={imageUrl} alt={name} className="rounded-lg mb-2" />
       <div className="mx-1">
         <div className="flex justify-between items-center">
