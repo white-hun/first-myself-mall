634f6512772adf74460a90609b82fec526816c1c
white-hun <121800818+white-hun@users.noreply.github.com>
2023-06-07 22:20:14 +0900

    Update Banner.jsx

--- first-myself-mall/src/components/Banner.jsx
+++ first-myself-mall/src/components/Banner.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Banner() {
   return (
-    <div className="m-5 w-7/12">
+    <div className="m-5 sm:w-10/12 lg:w-9/12 xl:w-7/12">
       <img src="/image/banner1.jpg" alt="bannerImage" />
     </div>
   );
